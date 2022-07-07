9b2dbb76e2d39c196d9d1e09b80abe709923755b
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 20:14:57 +0530

    New component added

---
+++ src/app/department-name/department-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-name',
+  templateUrl: './department-name.component.html',
+  styleUrls: ['./department-name.component.css']
+})
+export class DepartmentNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
