66c6842117fa0ad156ab9cf35410724792bd742d
xnjanx02 <anjana.jk009@gmail.com>
2022-07-07 20:22:59 +0530

    New component faculty added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentNameComponent } from './department-name/department-name.component';
+import { FacultyComponent } from './faculty/faculty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentNameComponent
+    DepartmentNameComponent,
+    FacultyComponent
   ],
   imports: [
     BrowserModule,
